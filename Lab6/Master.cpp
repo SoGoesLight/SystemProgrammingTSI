1ead77eefbbbc59d729fa0cd2d2187a0fec641c6
SoGoesLight <ilyalpt@gmail.com>
2020-05-07 09:36:14 +0300

    lab 6 finished

--- Lab6/Master.cpp
+++ Lab6/Master.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 // 14 % 10 + 1 = 5 вариант
 /*
-1. Написать программу SLAVE , которая выводит день недели по дате, переданной в качестве
+1. Написать программу SLAVE, которая выводит день недели по дате, переданной в качестве
 параметра.Если параметр не передан, вывести об этом сообщение и закончить программу.
 Если программа запущена из приложения MASTER, вывести на экран уникальный
 идентификатор приложения MASTER Если программа запущена сама по себе, вывести об
 этом информацию.
 
-2. Написать приложение MASTER которое:
+2. Написать приложение MASTER, которое:
 а) генерирует дату (день, месяц, год – 2000...2010), запускает приложение SLAVE и
 передает в качестве параметров сгенерированную дату и уникальный идентификатор
 процесса (MASTER). При завершении программы SLAVE завершается MASTER
@@ -32,42 +32,33 @@ int main()
 
     year = rand() % 11 + 2000; // 2000 < year < 2010 as requested by task
     month = rand() % 12 + 1;
-
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
         day = rand() % 31 + 1;
-
     if (month == 4 || month == 6 || month == 9 || month == 11)
         day = rand() % 30 + 1;
-
     if (month == 2 && year % 4 == 0)
         day = rand() % 29 + 1;
-
     if (month == 2 && year % 4 != 0)
         day = rand() % 28 + 1;
 
-    _cprintf("\n\nDate: %d/%d/%d\t(day/month/year)", day, month, year);
+    //_cprintf("\n\nDate: %d/%d/%d\t(day/month/year)", day, month, year);
 
-    char lpszAppName[] = "C:\\Projects\\SystemProgramming\\Debug\\Lab6_Slave.exe";
-    //char arguments[] = { (char)day, '/', (char)month, '/', (char)year, '\0'};
-
+    string AppNameAndArguments = "C:\\Projects\\SystemProgramming\\Debug\\Lab6_Slave.exe";
 
     STARTUPINFOA si;
     PROCESS_INFORMATION piApp;
 
-    //char date[256];
-    //sprintf_s(date, "%d %d %d ", day, month, year);
+    DWORD pid = GetCurrentProcessId();
+    //_cprintf("\n\nPID = %d", pid);
 
-    string cPath = "C:\\Projects\\SystemProgramming\\Debug\\Lab6_Slave.exe";
-    cPath += " " + to_string(day) + " " + to_string(month) + " " + to_string(year);
-
-    char* a = new char[cPath.length() + 1];
-    strcpy(a, cPath.c_str());
+    AppNameAndArguments += " " + to_string(day) + " " + to_string(month) + " " + to_string(year) + " " + to_string(pid);
+    char* args = new char[AppNameAndArguments.length()];
+    strcpy(args, AppNameAndArguments.c_str());
 
     ZeroMemory(&si, sizeof(STARTUPINFO));
     si.cb = sizeof(STARTUPINFO);
 
-    if (!CreateProcessA(lpszAppName, a, NULL, NULL, FALSE,
-        CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp)) {
+    if (!CreateProcessA(NULL, args, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp)) {
         _cputs("The new process is not created, check a name of the process.\n");
         _cputs("Press any key to finish.\n");
         _getch();
@@ -77,6 +68,8 @@ int main()
     _cputs("The new process is created.\n");
 
     WaitForSingleObject(piApp.hProcess, INFINITE);
+
+    cout << "All processes are finished\n\n";
 
     CloseHandle(piApp.hThread);
     CloseHandle(piApp.hProcess);
--- Lab6/Slave.cpp
+++ Lab6/Slave.cpp
@@ -1,17 +1,18 @@
 ﻿#include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
 // 14 % 10 + 1 = 5 вариант
 /*
-1. Написать программу SLAVE , которая выводит день недели по дате, переданной в качестве
+1. Написать программу SLAVE, которая выводит день недели по дате, переданной в качестве
 параметра.Если параметр не передан, вывести об этом сообщение и закончить программу.
 Если программа запущена из приложения MASTER, вывести на экран уникальный
 идентификатор приложения MASTER Если программа запущена сама по себе, вывести об
 этом информацию.
 
-2. Написать приложение MASTER которое:
+2. Написать приложение MASTER, которое:
 а) генерирует дату (день, месяц, год – 2000...2010), запускает приложение SLAVE и
 передает в качестве параметров сгенерированную дату и уникальный идентификатор
 процесса (MASTER). При завершении программы SLAVE завершается MASTER
@@ -20,21 +21,55 @@ using namespace std;
 выводиться достаточно подробная информация о ходе выполнения каждого из процессов.
 */
 
-int main(int argc, char *argv[])
+void printWeekDay(int day, int month, int year);
+
+int main(int argc, char* argv[])
 {
-    _cputs("I am created.");
-
-    _cputs("\n\nMy name is: ");
-    _cputs(argv[0]);
-    _cputs(argv[1]);
-    _cputs(argv[2]);
-    _cputs(argv[3]);
-
-    for (int i = 1; i < argc; ++i)
-        _cprintf("\n My %d parameter = %s", i, argv[i]);
+    if (argc > 1) {
+        DWORD parentalID = atoi(argv[4]);
+        _cprintf("\n\nExecuted by %d", parentalID);
+        _cprintf("\n\nDate is %s/%s/%s, and it is ", argv[1], argv[2], argv[3]);
+        printWeekDay(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    }
+    else
+        _cputs("\n\nExecuted by myself");
 
     _cputs("\n\nPress any key to finish.\n");
     _getch();
 
     return 0;
+}
+
+void printWeekDay(int day, int month, int year) {
+    int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    year -= month < 3;
+    int weekDay = (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
+
+    switch (weekDay) {
+    case 1:
+        _cputs("Monday");
+        break;
+    case 2:
+        _cputs("Tuesday");
+        break;
+    case 3:
+        _cputs("Wednesday");
+        break;
+    case 4:
+        _cputs("Thursday");
+        break;
+    case 5:
+        _cputs("Friday");
+        break;
+    case 6:
+        _cputs("Saturday");
+        break;
+    case 7:
+        _cputs("Sunday");
+        break;
+    default:
+        cout << weekDay;
+        _cputs("Something went terribly wrong");
+        break;
+    }
 }
