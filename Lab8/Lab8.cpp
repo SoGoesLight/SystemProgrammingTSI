114bdea09a74d81fda3483881474bfae5becf363
SoGoesLight <ilyalpt@gmail.com>
2020-05-24 22:52:37 +0300

    8th lab finished

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -21,57 +21,135 @@ int и float содержат индекс своей структуры в ма
 Вывести на экран предпоследние элементы каждого массива каждой структуры.
 
 */
-const int magicNumber = 500;
+
+const int arrayInStructSize = 500;
+const int arraySize = 10;
+const int numForShort = 10;
+short shortArr[arraySize];
 
 struct labStruct {
-    int intArr[magicNumber];
-    char charArr[magicNumber];
-    float floatArr[magicNumber];
+    int intArr[arrayInStructSize];
+    float floatArr[arrayInStructSize];
+    char charArr[arrayInStructSize];
 };
+
+labStruct myStruct[arraySize];
+const short shift = sizeof(short) * arraySize;
+
+void writingToMemoryShort(char* shiftPointer);
+void writingToMemoryStruct(SYSTEM_INFO sysInfo, char* pointer, char* pointerForSt);
+void readingFromMemory(char* pointerS, char* pointer, char* pointerForSt);
 
 int main()
 {
     srand(time(NULL));
     SYSTEM_INFO sysInfo;
-    LPVOID lp;
 
     GetSystemInfo(&sysInfo);
 
     printf("Hardware information: \n");
     printf("  Page size: %u\n", sysInfo.dwPageSize);
-    printf("  Allocation granularity: %u\n", sysInfo.dwAllocationGranularity);
+    printf("  Allocation granularity: %u\n\n", sysInfo.dwAllocationGranularity);
 
-    lp = VirtualAlloc(
-        NULL,                 // System selects address
-        sysInfo.dwPageSize,   // Size of allocation
-        MEM_RESERVE,          // Allocate reserved pages
-        PAGE_NOACCESS);       // Protection = no access
+    // Fulfilling with data
+    for (int i = 0; i < arraySize; i++) {
+        shortArr[i] = rand() % numForShort + 1;
 
-    if (lp == NULL)
-        printf("VirtualAlloc reserve failed.");
-
-    // 3 - fulfilling with data
-    short arr[10];
-    for (int i = 0; i < 10; i++)
-        arr[i] = rand() % 10 + 1;
-
-    labStruct myStruct[10];
-    for (int i = 0; i < 10; i++) {
-        myStruct[i].intArr[magicNumber - 1] = i;
-        myStruct[i].floatArr[magicNumber - 1] = i;
-        myStruct[i].charArr[magicNumber - 1] = (char)(97 + i);
+        myStruct[i].intArr[arrayInStructSize - 2] = i;
+        myStruct[i].floatArr[arrayInStructSize - 2] = i;
+        myStruct[i].charArr[arrayInStructSize - 2] = (char)(97 + i);
     }
 
-    // VirtualAlloc short arr
-    // VirtualAlloc myStruct
+    char* startPointer = (char*)VirtualAlloc(
+            NULL,                               // System selects address for start pointer
+            sysInfo.dwAllocationGranularity,    // Size of allocation
+            MEM_RESERVE,                        // Allocate reserved pages
+            PAGE_READWRITE);                    // Protection
 
-    // перед каждой записью сдвиг на arr[i]
+    startPointer = (char*)VirtualAlloc(startPointer, sysInfo.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
 
+    char* shiftPointer = startPointer;
 
+    if (shiftPointer == NULL) {
+        printf("VirtualAlloc failed.");
+    }
+    else {        
+        // Operations
+        printf("Write operations:\n");
+        printf("  Pointer created at 0x%p\n", shiftPointer);
 
+        writingToMemoryShort(shiftPointer);
 
-    if (!VirtualFree(lp, 0, MEM_RELEASE))
-        printf("Memory release failed."); 
+        char* structPointer = startPointer + shift;
+        shiftPointer = structPointer;
+        printf("\n  Struct pointer at 0x%p\n\n", structPointer);
+        structPointer = startPointer;
+
+        writingToMemoryStruct(sysInfo, shiftPointer, structPointer);
+        printf("\nRead operations:\n");
+        readingFromMemory(startPointer, shiftPointer, structPointer);
+    }
+
+    if (!VirtualFree(startPointer, 0, MEM_RELEASE))
+        printf("Memory release failed.");
 
 	return 0;
+}
+
+void writingToMemoryShort(char* shiftPointer) {
+    printf("\n  Array of shorts:\n");
+    for (int i = 0; i < arraySize; i++) {
+        *(short*)shiftPointer = shortArr[i];
+        shiftPointer += sizeof(short);
+        printf("    Pointer %d at 0x%p\n", i, shiftPointer);
+    }
+}
+
+void writingToMemoryStruct(SYSTEM_INFO sysInfo, char* shiftPointer, char* structPointer) {
+    for (int i = 0; i < arraySize; i++) {
+        shiftPointer += shortArr[i];
+
+        printf("  Pointer shifted, struct %d located at 0x%p\n", i, shiftPointer);
+
+        // Handling here
+        if (shiftPointer - structPointer + sizeof(labStruct) + arraySize > sysInfo.dwPageSize) {
+            structPointer = (char*)VirtualAlloc(shiftPointer + sysInfo.dwPageSize + 1, sysInfo.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+            if (structPointer == NULL) {
+                printf("  VirtualAlloc failed.");
+            }
+            else {
+                printf("    New page allocated\n");
+            }
+        }
+
+        *(labStruct*)shiftPointer = myStruct[i];
+        shiftPointer += sizeof(labStruct);
+    }
+}
+
+void readingFromMemory(char* startPointer, char* shiftPointer, char* structPointer) {
+    shiftPointer = startPointer;
+    structPointer = shiftPointer + shift;
+
+    for (int i = 0; i < arraySize; i++) {
+        short shortValue = *(short*)shiftPointer;
+
+        printf("  Data from short pointer at 0x%p\n", shiftPointer);
+        printf("    Short value: %d\n", shortValue);
+
+        structPointer += shortValue;
+        shiftPointer += sizeof(short);
+
+        int intValue = *(int*)(structPointer + arrayInStructSize * sizeof(int) - 2 * sizeof(int));
+        float floatValue = *(float*)(structPointer + arrayInStructSize * (sizeof(int) + sizeof(float)) - 2 * sizeof(float));
+        char charValue = *(char*)(structPointer + arrayInStructSize * (sizeof(int) + sizeof(float) + sizeof(char)) - 2 * sizeof(char));
+
+        printf("  Data from structure pointer at 0x%p", structPointer);
+        printf("\n    Int value: %d", intValue);
+        printf("\n    Float value: %f", floatValue);
+        printf("\n    Char value: %c\n", charValue);
+
+        structPointer += sizeof(labStruct);
+        printf("\n");
+    }
 }
